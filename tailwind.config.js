1b617f3a2c49ce7c42798d7c38ed24f2a1f39487
slobodan.erak <slobodan.erak@consulteer.com>
2021-02-15 16:54:01 +0100

    Adding TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: {
+    content: ["./app/**/*.html.erb"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
